870a3c2b11d9c29927e403dfd39d1da8e6b8c106
Lam Truong An <truongan990@gmail.com>
2026-08-08 11:56:54 +0700

    Fix intro message in Ticket struct; implement WeekTemperatures struct with methods for temperature management; complete Fibonacci function with memoization; update resizing test to assert new capacity.

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,12 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let n = n as usize;
+    let mut fib = vec![0, 1];
+    for i in 2..=n {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+    fib[n]
 }
 
 #[cfg(test)]
